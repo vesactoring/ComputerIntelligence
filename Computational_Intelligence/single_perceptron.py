e7b0c3b6655b5b0b5bec9b39a077b725a6244e18
Libby <L.Tang-2@student.tudelft.nl>
2022-02-24 20:34:39 +0100

    Combine the functions

--- Computational_Intelligence/single_perceptron.py
+++ Computational_Intelligence/single_perceptron.py
@@ -1,18 +1,23 @@
-import numpy as np
+def epoches(epoch, features, weights, threshold, labels, learning_rate):
+    for j in range(epoch):
+        for i in range(features.shape[0]):
+            actual = labels[i]
+            data = features[i]
 
-def sum(features, weights):
-    z = features @ weights
+            x0 = data[0]
+            x1 = data[1]
 
-def activate(z, threshold):
-    if(z > threshold):
-        return 1 
-    else：
-        return 0
+            z = x0 * weights[0] + x1 * weights[1]
 
-def loss(actual, predict):
-    return actual - predict
+            if(z > threshold):
+                a = 1 
+            else:
+                a = 0
+            
+            lose = actual - a
 
-def update_weights(loss, weights, features, learning_rate):
-    weights = weights + loss * features * learning_rate
+            weights = weights + learning_rate * lose * data
+
+        
 
 
