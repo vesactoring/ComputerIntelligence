f2fb560379c5fb902e8eb38e9d7efbe9bd189b96
Libby <L.Tang-2@student.tudelft.nl>
2022-02-26 13:55:13 +0100

    Get the plots working

--- Computational_Intelligence/main.py
+++ Computational_Intelligence/main.py
@@ -1,2 +1,12 @@
+import numpy as np
+from single_perceptron import plot
+
 if __name__ == '__main__':
-    print("Hello world!")
+    label_and = np.array([0, 0, 0, 1])
+    label_or = np.array([0, 1, 1, 1])
+    label_xor = np.array([0, 1, 1, 0])
+
+    max_epoch = 10
+    
+    plot = plot(max_epoch, label_xor)
+    
--- Computational_Intelligence/single_perceptron.py
+++ Computational_Intelligence/single_perceptron.py
@@ -1,21 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-features = [[0, 0], [1, 0], [0, 1], [1, 1]]
-label_and = [0, 0, 0, 1]
-label_or = [0, 1, 1, 1]
-label_xor = [0, 1, 1, 0]
+features = np.array([[0, 0, 1], [1, 0, 1], [0, 1, 1], [1, 1, 1]])
+label_and = np.array([0, 0, 0, 1])
+label_or = np.array([0, 1, 1, 1])
+label_xor = np.array([0, 1, 1, 0])
 
-weights = [1, 1]
+weights = [1, 0, 0]
 threshold = 0.5
 learning_rate = 0.1
+max_epoch = 10
 
 
 def sum(data, weights):
     x0 = data[0]
     x1 = data[1]
+    bias = data[2]
 
-    return x0 * weights[0] + x1 * weights[1]
+    return x0 * weights[0] + x1 * weights[1] + bias * weights[2]
 
 def activate(z, threshold):
     if(z > threshold):
@@ -34,10 +35,13 @@ def training(epoch, features, weights, threshold, labels, learning_rate):
             z = sum(data, weights)
 
             a = activate(z, threshold)
-            
             lose = actual - a
 
-            w = weights + learning_rate * lose * data
+            w[0] = w[0] + learning_rate * lose * data[0]
+            w[1] = w[1] + learning_rate * lose * data[1]
+            w[2] = w[2] + learning_rate * lose * data[2]
+
+
     return w
 
 
@@ -50,27 +54,31 @@ def predict(features, weights, threshold):
         z = sum(data, weights)
         
         result[i] = activate(z, threshold)
+    return result
 
 def error(predicts, labels):
-    return np.sum(np.square(labels- predicts)) / len(predict) 
+    return np.sum(np.square(labels- predicts)) / predicts.size
 
 
 def plot(max_epoch, labels):
     errors = []
-    epoches = np.linspace(0, 1, max_epoch)
+    epoches = np.linspace(1, max_epoch, max_epoch)
 
-    for i in max_epoch:
-        weights = training(i, features, weights, threshold, labels, learning_rate)
-        e = error(predict(features, weights, threshold), labels)
+    for i in range(max_epoch):
+        w = training(i, features, weights, threshold, labels, learning_rate)
+        e = error(predict(features, w, threshold), labels)
         errors.append(e)
 
-    _, axis = plt.subplots()
-    axis.plot(epoches, errors)
+    print(epoches)
+    print(errors)
+
+    # This is put to plotting.ipynb
+    # plt.plot(epochs, errors)
     
-    axis.legend()
-    axis.set_xlabel('epoch')
-    axis.set_ylabel('errors')
-    plt.title('errors for different epoch')
-    plt.grid()
-    plt.show()
+    # axis.legend()
+    # axis.set_xlabel('epoch')
+    # axis.set_ylabel('errors')
+    # plt.title('errors for different epoch')
+    # plt.grid()
+    # plt.show()
 
