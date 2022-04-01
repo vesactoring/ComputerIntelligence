5d7c3e601603536b6b0d1b4515e8d3332a96d831
Yi Wu <y.wu-37@student.tudelft.nl>
2022-04-01 18:49:54 +0200

    Added code to graph

--- QLearning-Python-master/main/RunMe.py
+++ QLearning-Python-master/main/RunMe.py
@@ -5,6 +5,7 @@ from Maze import Maze
 from Agent import Agent
 from mysolution.MyQLearning import MyQLearning
 from mysolution.MyEGreedy import MyEGreedy
+import matplotlib.pyplot as plt
 
 def run(maze, robot, selection, learn, max_steps, alpha, epsilon, gamma, decay= None):
     # keep learning until you decide to stop
@@ -28,11 +29,10 @@ def run(maze, robot, selection, learn, max_steps, alpha, epsilon, gamma, decay= 
             number_of_steps += 1
             # self, state, action, r, state_next, possible_actions, alfa, gamma
             reward = maze.get_reward(next_state)
-            # new_q = learn.update_q(state, action, reward, next_state, maze.get_valid_actions(robot), alpha, gamma)
+            new_q = learn.update_q(state, action, reward, next_state, maze.get_valid_actions(robot), alpha, gamma)
             
-            # learn.set_q(state, action, new_q)
-    print("trials: ", trials)
-    print("avg steps: ", sum_of_trial_steps / trials)
+            learn.set_q(state, action, new_q)
+    return trials, sum_of_trial_steps / trials
 
 
 
@@ -54,7 +54,7 @@ def run(maze, robot, selection, learn, max_steps, alpha, epsilon, gamma, decay= 
 if __name__ == "__main__":
     # load the maze
     # TODO replace this with the location to your maze on your file system
-    file = "../data/toy_maze.txt"
+    file = "../data/easy_maze.txt"
     maze = Maze(file)
 
     # Set the reward at the bottom right to 10
@@ -76,15 +76,29 @@ if __name__ == "__main__":
     total_steps_before_reset = 0
     max_steps = 30000
     trials = 0
-    epsilon = 0.01
-    alpha = 0.1
+    epsilon = 0.1
+    alpha = 0.8
     gamma = 0.7
     stop = False
     decay = 0.1
     
-    run(maze , robot, selection, learn, max_steps, alpha, epsilon, gamma)
+    x_lab = []
+    y = []
 
+    for i in range(10):
+        trials, avg = run(maze , robot, selection, learn, max_steps, alpha, epsilon, gamma)
+        x_lab.append(trials)
+        y.append(avg)
     
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    plt.xticks(ticks=x, labels=x_lab)
+    plt.xlabel("Amount of trials")
+    plt.ylabel("Average length per trial")
+    plt.title("Alpha: 0.8")
+    ax = plt.gca()
+    ax.set_ylim([0, 4000])
+    plt.plot(x, y)
+    plt.show
 
     
     
